5d2b5d4551716fc7eaa6c69b7ae926be594c77fd
Chris Hendricks <chris.hendricks@tylertech.com>
2025-04-21 13:54:03 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cwh-py",
+    packages=find_packages(where="cwh-py"),  # Look for packages inside cwh-py directory
+    package_dir={"": "cwh-py"},  # Tell setuptools where to find the packages
     version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        # List your dependencies here
-        # 'requests>=2.25.1',
-    ],
-    author="Chris Hendricks",
-    author_email="chris.hendricks@tylertech.com",
-    description="A collection of dev utility packages",
-    python_requires=">=3.6",
 )
